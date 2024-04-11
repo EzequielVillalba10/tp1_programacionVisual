9761459557c57d73e8cecf30f032bfc44f9efe3b
EzequielVillalba10 <ezequielvillagf10@gmail.com>
2024-04-10 21:47:59 -0300

    Primer commit, creacion de paquete y clase para ejercicio1

---
+++ tp1_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero1=10;
+		byte numero2=8;
+		short numero3=100;
+		long numero4=13;
+		float numero5=1.0f;
+		float resultado;
+		
+		resultado= numero1+numero2+numero3+numero4+numero5;
+		
+		System.out.println(resultado); //resultado de suma de los valores de las variables
+	}
+
+}
