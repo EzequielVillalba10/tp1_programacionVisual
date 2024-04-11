00f930afeb55d50c7da78dc2ad91950257acfd80
EzequielSykes <116243645+EzequielSykes@users.noreply.github.com>
2024-04-10 22:13:18 -0300

    Segundo Commit, creacion de paquete para ejercicio 2 y definicion de clase para ese mismo paquete.

---
+++ tp1_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,39 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// definicion de variables
+		String nombrePais;
+		int edad;
+		float alturaEdificio;
+		float precioProducto;
+		int numeroTelefono;
+		double calculoCoseno;
+		
+		// asignandole valores a las variables
+		
+		nombrePais="Brasil";
+		edad=22;
+		alturaEdificio=828;
+		precioProducto=99.99f;
+		numeroTelefono=388192813;
+		calculoCoseno=0.5;
+		
+		//Mostrando los valores por consola
+		
+		System.out.println("Nombre de un Pais: "+nombrePais);
+		System.out.println("Edad: "+edad);
+		System.out.println("Altura de un edificio como Burj Khalifa: "+alturaEdificio+" m.");
+		System.out.println("Precio de un producto: $"+precioProducto);
+		System.out.println("Numero de telefono: "+numeroTelefono);
+		System.out.println("Calculo de coseno: "+calculoCoseno);
+		
+		
+		
+		
+	}
+
+}
