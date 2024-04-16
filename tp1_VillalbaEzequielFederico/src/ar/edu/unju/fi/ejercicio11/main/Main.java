bbc004ac673c46513aa12c75909592ecbe9756a6
EzequielSykes <116243645+EzequielSykes@users.noreply.github.com>
2024-04-16 19:55:54 -0300

    Ejerccio 11 continuacion del tp1

---
+++ tp1_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio11/main/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio11.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=80;
+		
+		for (int j=40,i=2;j>0;j--,i++)
+		{
+			System.out.println(x + " ");
+			x += (j-i-1);
+			
+		}
+	}
+
+}
