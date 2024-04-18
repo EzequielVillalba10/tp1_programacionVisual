a9c088b5097494f09b213f01d1a4f26930aeff96
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-04-18 00:40:56 -0300

    Ejercicio 16 terminado....

--- tp1_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio16/Main.java
+++ tp1_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio16/Main.java
@@ -17,10 +17,31 @@ public class Main {
 		int j=0;
 		while(j<array.length)
 		{
-			System.out.println(array[j]);
+			System.out.println("Posicion: ["+j+"]  ||  Valor: "+array[j]);
 			j++;
 		}
 		System.out.println("El tamaño del array es: "+array.length);
+		
+		byte indice;
+		do
+		{
+			System.out.println("Ingrese indice para eliminar un alemento: ");
+			indice = scanner.nextByte();
+			
+			}while(indice<0 || indice >array.length);
+		for(int k=indice; k<array.length-1;k++)
+		{
+			array[k] = array[k+1];
+		}
+		array[array.length-1] = null;
+		System.out.println("////////////////////////////////////");
+		System.out.println("ARREGLO DESPUES DEL ELEMENTO ELIMINADO");
+		for (int l=0; l<array.length; l++)
+		{
+			System.out.println("Posicion: ["+l+"]  ||  Valor: "+array[l]);
+			
+		}
+		
 	}
 
 }
