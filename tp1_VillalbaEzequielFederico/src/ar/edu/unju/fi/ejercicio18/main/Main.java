c38991aa823d9449710eb2ccab0fc39e737e1c16
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-04-20 19:34:06 -0300

    Creacion de lista para paises y destinos turisticos

--- tp1_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio18/main/Main.java
+++ tp1_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio18/main/Main.java
@@ -1,10 +1,19 @@
 package ar.edu.unju.fi.ejercicio18.main;
 
+import java.util.ArrayList;
+
+import ar.edu.unju.fi.ejercicio18.model.DestinoTuristico;
+import ar.edu.unju.fi.ejercicio18.model.Pais;
+
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		ArrayList<Pais> paises = new ArrayList<>();
+		ArrayList<DestinoTuristico> destinos = new ArrayList<>();
 		
 	}
 
