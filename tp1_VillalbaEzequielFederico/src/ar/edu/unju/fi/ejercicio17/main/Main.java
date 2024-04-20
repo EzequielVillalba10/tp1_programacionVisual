a90db331d0611e915688f97ccc57a4f7dab9085b
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-04-20 16:15:09 -0300

    Creacion de lista jugadores y inicializacino de scanner

---
+++ tp1_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio17/main/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio17.main;
+
+import java.util.List;
+import java.util.Scanner;
+import ar.edu.unju.fi.ejercicio17.model.Jugador;
+
+public class Main {
+
+	
+	private static Scanner sc;
+	private static List<Jugador> jugadores; // lista creacion
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		sc = new Scanner(System.in); // creacion de objeto scanner
+	}
+
+}
