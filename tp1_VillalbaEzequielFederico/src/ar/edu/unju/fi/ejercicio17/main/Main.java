a035601f7dd0a59ae1b03898cc2fdc7a74aebf28
EzequielVIllalba10 <ezequielvillagf@gmail.com>
2024-04-20 16:46:40 -0300

    Creacion del array para Jugador, y opcion para cargar el array con datos.

--- tp1_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio17/main/Main.java
+++ tp1_VillalbaEzequielFederico/src/ar/edu/unju/fi/ejercicio17/main/Main.java
@@ -1,5 +1,6 @@
 package ar.edu.unju.fi.ejercicio17.main;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import ar.edu.unju.fi.ejercicio17.model.Jugador;
@@ -14,6 +15,89 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		sc = new Scanner(System.in); // creacion de objeto scanner
+		int opcion=0;
+		
+		do {
+			System.out.println("==================================");
+			System.out.println("1 - Alta de Jugador");
+			System.out.println("2 - Mostrar los datos del jugador ");
+			System.out.println("3 - Modificar los datos del jugador");
+			System.out.println("4 - Mostrar todos los jugadores ordenado por apellido");
+			System.out.println("5 - Eliminar un jugador");
+			System.out.println("6 - Mostrar la cantidad de jugadores que tiene la arrayList");
+			System.out.println("7 - Mostrar la cantidad de jugadores que pertenecen a una Nacionalidad");
+			System.out.println("8 - Salir");
+			System.out.println("==================================");
+			System.out.println("Ingrese una opcion: ");
+			opcion = sc.nextInt();
+			sc.nextLine(); // Borra el baffer del scanner
+			
+			switch (opcion)
+			{
+			case 1: precargarJugador();break;
+			
+			case 2:break;
+			
+			case 3:break;
+				
+			case 4:break;
+				
+			case 5:break;
+				
+			case 6:break;
+				
+			case 7:break;
+				
+			case 8: System.out.println("Saliendo del programa");
+				break;
+				default: System.out.println("OPCION INCORERECTA!!!!");
+				
+			}
+			}while(opcion!=8);
+		
+	}
+	
+	/**
+	 * Aca se dara de alta ingresando los datos del jugador para cargar en el arraylist
+	 */
+	public static void precargarJugador() {
+		int opcion1=0;
+		String posicion="ninguna";
+		System.out.println("Ingrese nombre de jugador: ");
+		String nombre = sc.next();
+		System.out.println("Ingrese apellido de jugador: ");
+		String apellido = sc.next();
+		System.out.println("Ingrese Nacionalidad de jugador: ");
+		String nacionalidad = sc.next();
+		System.out.println("Ingrese estatura del jugador: ");
+		double estatura = sc.nextDouble();
+		System.out.println("Ingrese peso del jugador: ");
+		double peso = sc.nextDouble();
+		System.out.println("Ingrese posicion: ");
+		do {
+			System.out.println("----------------------");
+			System.out.println("1)Delantero");
+			System.out.println("2)Defensa");
+			System.out.println("3)Medio");
+			System.out.println("4)Arquero");
+			System.out.println("-----------------------");
+			opcion1 = sc.nextInt();
+			if (opcion1 <1 || opcion1 >4)
+			{
+			System.out.println("ERROR, INGRESE UNA OPCION VALIDA!!!");	
+			}
+		}while(opcion1<1 || opcion1>4);
+		
+		switch (opcion1) {
+		case 1: posicion = "Delantero";break;
+		
+		case 2: posicion = "Defensa"; break;
+		
+		case 3: posicion = "Medio"; break;
+		
+		case 4: posicion = "Arquero"; break;
+		}
+		jugadores.add(new Jugador(nombre,apellido,nacionalidad,estatura,peso,posicion));
 	}
 
 }
